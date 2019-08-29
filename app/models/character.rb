792bc6297ef4c59eba6771b7a632ed88cc850020
Huy Do <huyddo40@gmail.com>
2019-08-29 16:50:35 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :shows
+  belongs_to :actors
+
+  def say_that_thing_you_say
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,4 @@
 class Show < ActiveRecord::Base
-  
+  def actors_list
+  end
 end
