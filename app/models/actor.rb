df49e5665cab7f331ff330ddf8ca5764de79987d
Huy Do <huyddo40@gmail.com>
2019-08-29 21:33:31 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,17 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    full_name +
-    binding.pry
+    char_show_name = []
+    self.characters.each do |char_name|
+      char_show_name << char_name.name
+
+    end
+
+    self.shows.each do |show_name|
+      char_show_name << show_name.name
+      binding.pry
+    end
+    char_show_name
   end
+
 end
