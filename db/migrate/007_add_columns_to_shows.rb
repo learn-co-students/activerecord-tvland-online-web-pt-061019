b8c34897e1bb2d40afdc8c4b3f974131279d8365
HuydDo <huydinhdo@yahoo.com>
2019-08-29 23:09:27 -0600

    Done.

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -1,7 +1,4 @@
-class AddColumnsToShows < ActiveRecord::Migration[5.1]
-  #to make an additive change to a schema we create
-  #a new migration, and then in the change method,
-  #we make the change.
+class AddColumnsToShows < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
