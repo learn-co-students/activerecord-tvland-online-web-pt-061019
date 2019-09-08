a46a85298412080299a5049654e3412ce5907db6
scrabill <shannon.crabill@gmail.com>
2019-09-08 18:36:17 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,16 +3,22 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.first_name
-    self.last_name
+    "#{self.first_name} #{self.last_name}"
   end
 
   def list_roles
-    Character.all.map do |character|
-      characters.name
-      shows.name
-    end
+    "#{self.characters.first.name} - #{self.shows.first.name}"
   end
+
+  # peter = Actor.new(:first_name => "Peter", :last_name => "Dinklage")
+  # tyrion = Character.new(:name => "Tyrion Lannister")
+  # tyrion.actor = peter
+  # thrones = Show.new(:name => "Game of Thrones")
+  # tyrion.show = thrones
+  # tyrion.save
+
+  # ['Tyrion Lannister - Game of Thrones']
+
 end
 
 
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,17 +1,36 @@
 class Show < ActiveRecord::Base
+  has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
-  
-
-
 
   def actors_list
     # self.idea
 
-    Actor.find_by("id = ?", self.actor_id).map do |actor|
-      puts "#{actor.first_name} + " " + #{actor.last_name}"
-    end
+    # Actor.find_by("id = ?", self.actor_id).map do |actor|
+    #   "#{actor.first_name} + " " + #{actor.last_name}"
+    # end
+    # actor.id == character.actor_id
+
+
+    # Character.where("actor_id = ?", self.id).map do |actor|
+    #   "#{actor.full_name} + " " + #{actor.show.name}"
+    # end
+
+    self.shows
+
+    # ("Khaleesi - Game of Thrones")
   end
+
+  def characters
+    # self.id
+    # self.characters
+    # Character.where("show_id = ?", self.id).name.first
+    # Show.where("id = ?", self.id)
+    Character.where("show_id = ?", self.id)
+  end
+
+
+
 
   # def full_name
   #   self.first_name
