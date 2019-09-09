116cd31352433fa244c55e39bb48c12e3068c0dc
scrabill <shannon.crabill@gmail.com>
2019-09-08 20:20:28 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,36 +1,14 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  has_many :actors, through: :characters
+  has_many :actors, through: :characters # Gives self.actors method built in (hey, look at this shows characters)
   belongs_to :network
 
   def actors_list
-    # self.idea
 
-    # Actor.find_by("id = ?", self.actor_id).map do |actor|
-    #   "#{actor.first_name} + " " + #{actor.last_name}"
-    # end
-    # actor.id == character.actor_id
+    self.actors.map do |actor|
+        actor.full_name
+    end
 
-
-    # Character.where("actor_id = ?", self.id).map do |actor|
-    #   "#{actor.full_name} + " " + #{actor.show.name}"
-    # end
-
-    self.shows
-
-    # ("Khaleesi - Game of Thrones")
-  end
-
-  def characters
-    # self.id
-    # self.characters
-    # Character.where("show_id = ?", self.id).name.first
-    # Show.where("id = ?", self.id)
-    # Character.where("show_id = ?", self.id)
-    # self.idcharacters
-    Characters.where("character.show_id = ?", self.id)
-    # self.id # this show
-    # Character.show_id # the character we want
   end
 
 end
