dd1d03e4fee2a6ac6a07cae9dd1703e6acb9d421
Khris Punzalan <khrispunzalan@gmail.com>
2019-09-10 17:23:02 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 2019_09_10_234806) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 7) do
     t.string "day"
     t.string "genre"
     t.string "season"
+    t.string "time"
   end
 
 end
