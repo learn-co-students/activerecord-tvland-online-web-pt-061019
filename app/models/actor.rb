4d32c736b6911d99640a01bd972fc2a9d8e9556e
Aida Abdikulova <abdikulova@gmail.com>
2019-10-01 01:18:15 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    person = self.characters.map do |value|
+      value.name
+    end
+    tv = self.shows.map do |values|
+      values.name
+    end
+    "#{person.join}" " - " "#{tv.join}"
+  end
+
 end
